adc63d4397a1b695e14a6baa4cbde8caa31c8e12
sakibhasansufi <sakibhasansufi@gmail.com>
2024-02-29 16:57:59 +0600

    button disabled

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ for (const seat of allSeat) {
         const seatName = e.target.parentNode.childNodes[1].innerText;
         const className = 'Economy';
         let price = 550;
-
+        this.style.pointerEvents = 'none';
+        this.classList.add('disabled');
 
         if (totalClick >= 4) {
             this.disabled = true;
@@ -70,12 +71,13 @@ for (const seat of allSeat) {
     });
 }
 
-document.getElementById('apply-button').addEventListener('click', function () {
-    console.log('clicked');
+// for disabling the apply button
+document.getElementById('apply').addEventListener('click', function () {
     const couponElement = document.getElementById('input-field').value;
     const couponCode = couponElement;
     const price = 0;
     console.log(couponCode);
+
     if (couponCode === 'NEW15') {
         const grandTotal = document.getElementById('grand-total').innerText;
         const convertGrandTotal = parseInt(grandTotal);
@@ -85,7 +87,6 @@ document.getElementById('apply-button').addEventListener('click', function () {
         this.remove()
 
     }
-
     else if (couponCode === 'Couple 20') {
         const grandTotal = document.getElementById('grand-total').innerText;
         const convertGrandTotal = parseInt(grandTotal);
@@ -95,14 +96,12 @@ document.getElementById('apply-button').addEventListener('click', function () {
         this.remove()
 
     }
-
-
     else {
-        alert('wrong coupon');
+        // alert('wrong coupon');
         document.getElementById('input-field').value = '';
     }
-
 })
+
 
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
@@ -126,29 +125,3 @@ function showElementById(elementId2) {
     const element2 = document.getElementById(elementId2);
     element2.classList.remove('hidden');
 }
-
-
-
-const inputField = document.getElementById('myInput');
-const submitButton = document.getElementById('submitButton');
-
-
-submitButton.addEventListener('click', function (event) {
-    if (!inputField.checkValidity() || inputField.value.trim() === '') {
-        event.preventDefault(); 
-
-        if (!inputField.checkValidity()) {
-            inputField.focus();
-            alert('Please fill in the required field.');
-        } else {
-            alert('Please enter some text in the input field.');
-        }
-    }
-});
-
-
-
-
-
-
-
